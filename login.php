1101724de5a12f7e5361edcb9f25d6b1316ee1da
Sean Dylan Patterson <sean@seanpatterson.co.za>
2023-04-13 03:26:31 +0200

    init

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+// Connect to database
+$servername = " sql311.epizy.com ";
+$username = "epiz_33999353";
+$password = " H4MAJzdD7n78jB ";
+$dbname = "epiz_33999353_letsbowl";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Insert form data into database
+$email = $_POST['email'];
+$userpass = $_POST['userpass'];
+
+$sql = "SELECT * FROM userdetails WHERE email = '$email' AND userpass = '$userpass';
+";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+  $profile = $sql;
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+mkdir .profile;
+copy .profile/profile.js 
+$conn->close();
+header("Location: ./profile.html");
+?>
---
+++ signup.php
@@ -0,0 +1,29 @@
+<?php
+// Connect to database
+$servername = " sql311.epizy.com ";
+$username = "epiz_33999353";
+$password = " H4MAJzdD7n78jB ";
+$dbname = "epiz_33999353_letsbowl";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Insert form data into database
+$email = $_POST['email'];
+$userpass = $_POST['userpass'];
+
+$sql = "INSERT INTO userdetails (id,email,userpass,timestamp) VALUES (NULL,'$email','$userpass',timestamp());";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+header("Location: ./login.html");
+?>
